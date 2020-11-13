dd03933673f8baa3122fa8fc2690a5bf5694d9a5
Riza Nafis <riza.nafis@gmail.com>
2020-11-13 18:12:00 +0700

    Connect Authentication Fixes #23

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,10 +4,7 @@ import { api } from "./api";
 export const login = (data) => {
   Cookie.set("token", data.token, { expires: 0.04166 });
   Cookie.set("token_type", data.token_type);
-  api.interceptors.request.use((request) => {
-    api.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
-    return request;
-  });
+  api.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
 };
 
 export const logout = () => {
