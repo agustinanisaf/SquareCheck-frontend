2c06065dd7540dd76b8a00e55d912c9d0d0671d4
rizabach <riza.bachtiar.rb@gmail.com>
2020-11-17 15:37:28 +0700

    Guard Endpoint with Role from API Fixes #32

--- src/components/componentRoutes.js
+++ src/components/componentRoutes.js
@@ -3,7 +3,7 @@ import {Route, Redirect} from 'react-router-dom'
 import {isLogin} from '../utils/auth'
 import cookie from 'js-cookie'
 
-export const PrivateRoute = ({Component, ...attr}) => {
+export const PrivateRoute = ({component : Component, ...attr}) => {
   
   return (
     <Route
@@ -19,15 +19,15 @@ export const PrivateRoute = ({Component, ...attr}) => {
   );
 }
 
-export const PublicRoute = ({ Component,restricted, ...attr }) => {
+export const PublicRoute = ({ component: Component, restricted, ...attr }) => {
   return (
     <Route
       {...attr}
       render={(props) =>
-        !isLogin() ? restricted (
-          <Component {...props} />
+        !isLogin() && restricted ? (
+          (<Component {...props} />)
         ) : (
-          <Redirect to={{pathname: "/home", from: props.location}} />
+          <Redirect to={{ pathname: "/home", from: props.location }} />
         )
       }
     />
