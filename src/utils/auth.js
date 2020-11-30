90e459c8f93e0837a6c3b44d163374006727cb82
rizabach <riza.bachtiar.rb@gmail.com>
2020-11-30 11:26:53 +0700

    Unable to Logout Fixes #48

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,19 +3,30 @@ import { api } from "./api";
 
 export const login = (data) => {
   Cookie.set("token", data.token);
-  localStorage.setItem("token", data.token);
+  localStorage.setItem("expireToken", new Date().getTime());  
   api.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
 };
 
 export const logout = () => {
   Cookie.remove("token");
   delete api.defaults.headers.common["Authorization"];
-  // Cookie.remove("user");
 };
 
 export const isLogin = () => {
   if (Cookie.get("token")) {
+    if (isExpire()) {
+      logout();
+      return false;
+    }
     return true;
   }
   return false;
 };
+
+const isExpire = () => {
+  const now = new Date();
+  let expire = now.getTime() - localStorage.getItem("expireToken");
+  expire = Math.round(expire / 60000);
+
+  return expire > 60 ? true : false;
+}
