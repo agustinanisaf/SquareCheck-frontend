4d99f2df06f6ceec538f4e1f3fd6d69de674c717
rizabach <riza.bachtiar.rb@gmail.com>
2020-12-08 21:26:45 +0700

    Unable to Logout Fixes #48

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,31 +2,27 @@ import Cookie from "js-cookie";
 import { api } from "./api";
 
 export const login = (data) => {
-  Cookie.set("token", data.token);
-  localStorage.setItem("expireToken", new Date().getTime());  
+  Cookie.set("tokenSquareCheck", data.token, {expires : 0.041666});
+  // localStorage.setItem("expireToken", new Date().getTime());  
   api.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
 };
 
 export const logout = () => {
-  Cookie.remove("token");
+  Cookie.remove("tokenSquareCheck");
   delete api.defaults.headers.common["Authorization"];
 };
 
 export const isLogin = () => {
-  if (Cookie.get("token")) {
-    if (isExpire()) {
-      logout();
-      return false;
-    }
+  if (Cookie.get("tokenSquareCheck")) {
     return true;
   }
   return false;
 };
 
-const isExpire = () => {
-  const now = new Date();
-  let expire = now.getTime() - localStorage.getItem("expireToken");
-  expire = Math.round(expire / 60000);
+// const isExpire = () => {
+//   const now = new Date();
+//   let expire = now.getTime() - localStorage.getItem("expireToken");
+//   expire = Math.round(expire / 60000);
 
-  return expire > 60 ? true : false;
-}
+//   return expire > 60 ? true : false;
+// }
