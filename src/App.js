82c5dad7b34699b779f9869f8b7e027ab8071bed
rizabach <riza.bachtiar.rb@gmail.com>
2020-12-19 15:32:03 +0700

    Fix Error Redirect Page Fixes #57

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route, BrowserRouter as Router, Redirect } from "react-router-dom";
 import "./App.css";
 import {
   createMuiTheme,
@@ -75,6 +75,9 @@ function App() {
               </Switch>
             </Layout>
           </Route>
+          <Route path="/" render={() => (
+            <Redirect to="/home"/>
+          )}/>
           <Route path="*" component={ErrorPage} />
         </Switch>
       </Router>
